54cb1b19187a4153f7f916b87ee8a13ed29d922f
robderon <ben@1org.org>
2021-03-02 16:21:30 +0100

    Add files via upload

---
+++ NodeMongo/testmongo.js
@@ -0,0 +1,33 @@
+const { MongoClient } = require('mongodb');
+const DATABASE_NAME = 'test';
+const DB_URI = "mongodb://mongows:27017/${DATABASE_NAME}";
+const client = new MongoClient(DB_URI);
+async function main(){
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+        // just a test to see if it's working
+        let adminDb = client.db("admin")
+        await adminDb.command({ ping: 1 });
+        console.log("Connected successfully to server");
+        // print list of databases available to console.
+        let listdb = await  adminDb.command( { listDatabases: 1 } );
+        console.log(listdb)
+
+    } catch (e) {
+        console.error(e);
+    }
+}
+main().catch(console.error);
+
+
+
+
+function addFigures(figures) {
+    const result = coll.insertMany(figures);
+    console.log(`Inserted ${result.insertedCount} figures.`);
+}
